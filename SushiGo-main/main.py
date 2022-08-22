a1e5ce6c508062fb9bd20743a286bbe2b8f913ea
Cat Khanh (CK) <87422489+khanhha1005@users.noreply.github.com>
2022-08-22 14:08:41 +0700

    Add files via upload

--- SushiGo-main/main.py
+++ SushiGo-main/main.py
@@ -2,4 +2,4 @@ from env import get_list_action
 from env import amount_action as amount_action_space
 from env import  normal_main
 from env import check_victory
-from env import random_player
+from env import player_random as random_player
